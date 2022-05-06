3fe98173b9240436938e494f4914cfa07a4b6a24
unknown <ebteshamhossain98@gmail.com>
2022-05-06 16:26:57 +0600

    first commit

---
+++ contracts/PickupLines.sol
@@ -0,0 +1,16 @@
+/*Use a license depending on your project.*/
+//SPDX=License-Identifier: UNLICENSED
+
+// code is written for Solidity version 0.4.16, or a newer version
+pragma solidity ^0.8.0;
+
+// Built in Hardhat interactive Javascript console
+import "hardhat/console.sol";
+
+// Main solidity contract
+contract PickupLines {
+    // constructor function for our contract
+    constructor() {
+        console.log("I am the Cheesy PickUp Lines' smart contract.");
+    }
+}
